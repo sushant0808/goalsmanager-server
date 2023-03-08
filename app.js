87b73c72b27a23404f347137380290d6eb9a561c
Sushant <sushantmore09072000@gmail.com>
2023-03-08 23:14:19 +0530

    misc

--- app.js
+++ app.js
@@ -16,17 +16,6 @@ app.use(cors({
 }))
 
 
-// app.get('/*', function (req, res) {
-//     console.log('__dirname',__dirname)
-//     console.log('ok',path.join(__dirname, '../client/public/index.html'));
-//     res.sendFile(path.join(__dirname, '../client/public/index.html'), function (err) {
-//         if (err) {
-//             res.status(500).send(err)
-//         }
-//     })
-// })
-
-
 app.use(cookieParser());
 
 app.use(express.json())
@@ -36,7 +25,21 @@ app.get("/home", (req, res) => {
     res.send('Executed...');
 })
 
+app.get('/*', function (req, res) {
+    console.log('__dirname', __dirname)
+    console.log('ok', path.join(__dirname, '../client/public/index.html'));
+    res.sendFile(path.join(__dirname, '../client/public/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+
+    // res.redirect("https://task-manager-nt2i.onrender.com");
+})
+
 app.use("/", userRoute);
+
+
 
 mongoose.connect(process.env.DB)
     .then(() => {
@@ -47,5 +50,5 @@ mongoose.connect(process.env.DB)
     })
 
 app.listen(process.env.PORT || 8002, () => {
-    console.log('Server running on port',process.env.PORT || 8002);
+    console.log('Server running on port', process.env.PORT || 8002);
 })
