47992a5fa0fd51fb9ac55e173a0586257ac907dc
Sushant <sushantmore09072000@gmail.com>
2023-08-16 22:53:44 +0530

    misc

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const userRoute = require("./routes/UserRoute");
@@ -20,40 +19,38 @@ const path = require("path");
 //     })
 // })
 
-
 app.use(cookieParser());
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/home", (req, res) => {
-    res.send('Executed...');
-})
-
-app.get('/*', function (req, res) {
-    console.log('__dirname', __dirname)
-    console.log('ok', path.join(__dirname, '../client/public/index.html'));
-    res.sendFile(path.join(__dirname, '../client/public/index.html'), function (err) {
-        if (err) {
-            res.status(500).send(err)
-        }
-    })
-
-    // res.redirect("https://task-manager-nt2i.onrender.com");
-})
+  res.send("Executed...");
+});
 
 app.use("/", userRoute);
 
+// app.get('/*', function (req, res) {
+//     console.log('__dirname', __dirname)
+//     console.log('ok', path.join(__dirname, '../client/public/index.html'));
+//     res.sendFile(path.join(__dirname, '../client/public/index.html'), function (err) {
+//         if (err) {
+//             res.status(500).send(err)
+//         }
+//     })
 
+//     // res.redirect("https://task-manager-nt2i.onrender.com");
+// })
 
-mongoose.connect(process.env.DB)
-    .then(() => {
-        console.log("Database connected.....")
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("Database connected.....");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(process.env.PORT || 8002, () => {
-    console.log('Server running on port', process.env.PORT || 8002);
-})
+  console.log("Server running on port", process.env.PORT || 8002);
+});
